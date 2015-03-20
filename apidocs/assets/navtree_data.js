1944f7782de09b9d54d9b42303112efc4b10e1fd
Eduardo Macarron <eduardo.macarron@gmail.com>
2015-03-20 07:22:26 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "org.mybatis.caches.redis", "org/mybatis/caches/redis/package-summary.html", [ [ "Interfaces", null, [ [ "RedisCallback", "org/mybatis/caches/redis/RedisCallback.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "ConfigWithHost", "org/mybatis/caches/redis/ConfigWithHost.html", null, "" ], [ "LoggingRedisCache", "org/mybatis/caches/redis/LoggingRedisCache.html", null, "" ], [ "RedisCache", "org/mybatis/caches/redis/RedisCache.html", null, "" ], [ "SerializeUtil", "org/mybatis/caches/redis/SerializeUtil.html", null, "" ], [ "StringUtils", "org/mybatis/caches/redis/StringUtils.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,10 @@
+var DATA = [
+      { id:0, label:"org.mybatis.caches.redis", link:"org/mybatis/caches/redis/package-summary.html", type:"package" },
+      { id:1, label:"org.mybatis.caches.redis.ConfigWithHost", link:"org/mybatis/caches/redis/ConfigWithHost.html", type:"class" },
+      { id:2, label:"org.mybatis.caches.redis.LoggingRedisCache", link:"org/mybatis/caches/redis/LoggingRedisCache.html", type:"class" },
+      { id:3, label:"org.mybatis.caches.redis.RedisCache", link:"org/mybatis/caches/redis/RedisCache.html", type:"class" },
+      { id:4, label:"org.mybatis.caches.redis.RedisCallback", link:"org/mybatis/caches/redis/RedisCallback.html", type:"class" },
+      { id:5, label:"org.mybatis.caches.redis.SerializeUtil", link:"org/mybatis/caches/redis/SerializeUtil.html", type:"class" },
+      { id:6, label:"org.mybatis.caches.redis.StringUtils", link:"org/mybatis/caches/redis/StringUtils.html", type:"class" }
+
+    ];
