17eca066b63e6bc30007862109ffb0eac73c0e77
Jeremy Landis <jeremylandis@hotmail.com>
2015-10-11 01:14:57 -0400

    Added missing overrides

--- src/main/java/org/mybatis/caches/redis/DummyReadWriteLock.java
+++ src/main/java/org/mybatis/caches/redis/DummyReadWriteLock.java
@@ -27,36 +27,44 @@ class DummyReadWriteLock implements ReadWriteLock {
 
     private Lock lock = new DummyLock();
 
+    @Override
     public Lock readLock() {
         return lock;
     }
 
+    @Override
     public Lock writeLock() {
         return lock;
     }
 
     static class DummyLock implements Lock {
 
+        @Override
         public void lock() {
             // Not implemented
         }
 
+        @Override
         public void lockInterruptibly() throws InterruptedException {
             // Not implemented
         }
 
+        @Override
         public boolean tryLock() {
             return true;
         }
 
+        @Override
         public boolean tryLock(long paramLong, TimeUnit paramTimeUnit) throws InterruptedException {
             return true;
         }
 
+        @Override
         public void unlock() {
            // Not implemented
         }
 
+        @Override
         public Condition newCondition() {
             return null;
         }
