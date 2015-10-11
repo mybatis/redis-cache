3fff21a4d679957d68548f038af4d269bc9ddcfd
Jeremy Landis <jeremylandis@hotmail.com>
2015-10-11 00:50:34 -0400

    wagon-git commit

--- apidocs/assets/navtree_data.js
+++ apidocs/assets/navtree_data.js
@@ -1,5 +1,5 @@
 var NAVTREE_DATA =
-[ [ "org.mybatis.caches.redis", "org/mybatis/caches/redis/package-summary.html", [ [ "Description", "org/mybatis/caches/redis/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "RedisCallback", "org/mybatis/caches/redis/RedisCallback.html", null, "" ] ]
+[ [ "org.mybatis.caches.redis", "org/mybatis/caches/redis/package-summary.html", [ [ "Interfaces", null, [ [ "RedisCallback", "org/mybatis/caches/redis/RedisCallback.html", null, "" ] ]
 , "" ], [ "Classes", null, [ [ "ConfigWithHost", "org/mybatis/caches/redis/ConfigWithHost.html", null, "" ], [ "RedisCache", "org/mybatis/caches/redis/RedisCache.html", null, "" ], [ "SerializeUtil", "org/mybatis/caches/redis/SerializeUtil.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
