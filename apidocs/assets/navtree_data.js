a9545531f975b07bc8e3d124f1b32478a6ebed1b
Eduardo Macarron <eduardo.macarron@gmail.com>
2015-12-28 18:35:10 +0100

    wagon-git commit

--- apidocs/assets/navtree_data.js
+++ apidocs/assets/navtree_data.js
@@ -1,6 +1,6 @@
 var NAVTREE_DATA =
 [ [ "org.mybatis.caches.redis", "org/mybatis/caches/redis/package-summary.html", [ [ "Interfaces", null, [ [ "RedisCallback", "org/mybatis/caches/redis/RedisCallback.html", null, "" ] ]
-, "" ], [ "Classes", null, [ [ "ConfigWithHost", "org/mybatis/caches/redis/ConfigWithHost.html", null, "" ], [ "RedisCache", "org/mybatis/caches/redis/RedisCache.html", null, "" ], [ "SerializeUtil", "org/mybatis/caches/redis/SerializeUtil.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "RedisCache", "org/mybatis/caches/redis/RedisCache.html", null, "" ], [ "RedisConfig", "org/mybatis/caches/redis/RedisConfig.html", null, "" ], [ "SerializeUtil", "org/mybatis/caches/redis/SerializeUtil.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
