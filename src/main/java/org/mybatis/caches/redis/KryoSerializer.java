f140379e63d41edc1d3e1bccc6c933f4b037c7ff
suzhiwu <suzhiwu@kuaishou.com>
2021-05-08 21:53:40 +0800

    fix kryo thread-unsafe

--- src/main/java/org/mybatis/caches/redis/KryoSerializer.java
+++ src/main/java/org/mybatis/caches/redis/KryoSerializer.java
@@ -31,9 +31,14 @@ public enum KryoSerializer implements Serializer {
   // Enum singleton, which is preferred approach since Java 1.5
   INSTANCE;
 
-  private Kryo kryo;
-  private Output output;
-  private Input input;
+  /**
+   * kryo is thread-unsafe, use ThreadLocal.
+   */
+  private ThreadLocal<Kryo> kryos = ThreadLocal.withInitial(() -> {
+    Kryo kryo = new Kryo();
+    return kryo;
+  });
+
   /**
    * Classes which can not resolved by default kryo serializer, which occurs very
    * rare(https://github.com/EsotericSoftware/kryo#using-standard-java-serialization) For these classes, we will use
@@ -49,23 +54,20 @@ public enum KryoSerializer implements Serializer {
   private Serializer fallbackSerializer;
 
   private KryoSerializer() {
-    kryo = new Kryo();
-    output = new Output(200, -1);
-    input = new Input();
     unnormalClassSet = new HashSet<Class<?>>();
     unnormalBytesHashCodeSet = new HashSet<Integer>();
     fallbackSerializer = JDKSerializer.INSTANCE;// use JDKSerializer as fallback
   }
 
   public byte[] serialize(Object object) {
-    output.clear();
+    Output output = new Output(200, -1);
     if (!unnormalClassSet.contains(object.getClass())) {
       /**
        * In the following cases: 1. This class occurs for the first time. 2. This class have occurred and can be
        * resolved by default kryo serializer
        */
       try {
-        kryo.writeClassAndObject(output, object);
+        kryos.get().writeClassAndObject(output, object);
         return output.toBytes();
       } catch (Exception e) {
         // For unnormal class occurred for the first time, exception will be thrown
@@ -79,6 +81,7 @@ public enum KryoSerializer implements Serializer {
   }
 
   public Object unserialize(byte[] bytes) {
+    Input input = new Input();
     if (bytes == null) {
       return null;
     }
@@ -90,7 +93,7 @@ public enum KryoSerializer implements Serializer {
        */
       try {
         input.setBuffer(bytes);
-        return kryo.readClassAndObject(input);
+        return kryos.get().readClassAndObject(input);
       } catch (Exception e) {
         // For unnormal bytes occurred for the first time, exception will be thrown
         unnormalBytesHashCodeSet.add(hashCode);
