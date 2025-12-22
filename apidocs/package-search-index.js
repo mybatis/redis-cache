8b45bbcdbb34ea24573c7f3b8fa225b3a993f12a
hazendaz <jeremylandis@hotmail.com>
2025-12-22 02:41:12 +0000

    Deploying to gh-pages from @ mybatis/redis-cache@d5a5f893aee8dd2787a7eaf29925fb3b822114b7 🚀

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.mybatis.caches.redis"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.mybatis.caches.redis"}];updateSearchResults();
